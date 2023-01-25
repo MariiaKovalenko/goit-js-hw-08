6f618909903a24bdce630a36dd7233a9722ff911
MariiaKovalenko <dr.kovalenkomariia@gmail.com>
2023-01-25 18:07:09 +0200

    recommit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,11 +5,9 @@ const player = new Player(iframe);
 
 player.on("timeupdate", throttle(function (data) {
     const time = data.seconds;
-    console.log(time);
     localStorage.setItem("videoplayer-current-time", time);
     console.log(localStorage.getItem("videoplayer-current-time"));
 },1000));
 
 let TIME_DATA_PLAYER = localStorage.getItem("videoplayer-current-time");
-console.log(TIME_DATA_PLAYER);
 player.setCurrentTime(TIME_DATA_PLAYER);
