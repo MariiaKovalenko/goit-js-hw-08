1109be3c01a6dd4129b1bce3e06b73e4fe14a480
MariiaKovalenko <dr.kovalenkomariia@gmail.com>
2023-01-29 01:30:31 +0200

    corection 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,12 @@
+import throttle from 'lodash/throttle';
 
-import throttle from "lodash/throttle";
-let selectedElem = {};
-const LOCALSTORAGE_KEY = "feedback-form-state";
-let localObject = {email:' ', message:' '};
-
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let localObject = { email: ' ', message: ' ' };
 
 const formElem = {
-    form: document.querySelector('.feedback-form'),
-    input: document.querySelector('.feedback-form  input'),
-    textarea: document.querySelector('.feedback-form  textarea'),
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form  input'),
+  textarea: document.querySelector('.feedback-form  textarea'),
 };
 
 formElem.form.addEventListener('input', throttle(formData, 500));
@@ -17,29 +15,27 @@ formElem.form.addEventListener('submit', formSubmit);
 initForm();
 
 function formData(e) {
-    localObject[e.target.name] = e.target.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localObject));
-};
+  localObject[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localObject));
+}
 
 function formSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    console.log(savedData);
+  const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  console.log(savedData);
 
-    e.currentTarget.reset();
-    localStorage.removeItem(LOCALSTORAGE_KEY);
-    selectedElem = {};
-};
-
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  localObject = {};
+}
 
 function initForm() {
-    const persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
-    if (persistedFilters) {
-        selectedElem = JSON.parse(persistedFilters);
-        console.log(selectedElem);
-        formElem.input.value = selectedElem.email;
-        formElem.textarea.value = selectedElem.message;
-    };
-  
-};
+  const persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (persistedFilters) {
+    localObject = JSON.parse(persistedFilters);
+    console.log(localObject);
+    formElem.input.value = localObject.email;
+    formElem.textarea.value = localObject.message;
+  }
+}
